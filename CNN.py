15c57e75970efd8c5f674beb118281ceb00cd0f5
borderless-shu <68730849+borderless-shu@users.noreply.github.com>
2020-07-24 19:26:06 +0800

    Add files via upload

--- CNN.py
+++ CNN.py
@@ -1,1 +1,106 @@
-#This is the code for the CNN model
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+args={}
+kwargs={}
+args['batch_size']=1000
+args['test_batch_size']=1000
+args['epochs']=10  #The number of Epochs is the number of times you go through the full dataset.
+args['lr']=0.01 #Learning rate is how fast it will decend.
+args['momentum']=0.5 #SGD momentum (default: 0.5) Momentum is a moving average of our gradients (helps to keep direction).
+
+args['seed']=1 #random seed
+args['log_interval']=10
+args['cuda']=False
+
+#load the data
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./task1/corr_based_prediction-master/datasets/', train=True, download=False,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=args['batch_size'], shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./task1/corr_based_prediction-master/datasets/', train=False, download=False,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=args['test_batch_size'], shuffle=True, **kwargs)
+
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+
+class Net(nn.Module):
+  def __init__(self):
+      super(Net, self).__init__()
+
+      self.cnn_layers = nn.Sequential(
+          # Defining a 2D convolution layer
+          nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
+          nn.BatchNorm2d(4),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=2, stride=2),
+          # Defining another 2D convolution layer
+          nn.Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
+          nn.BatchNorm2d(4),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(kernel_size=2, stride=2),
+      )
+
+      self.linear_layers = nn.Sequential(
+          nn.Linear(4 * 7 * 7, 10)
+      )
+
+  # Defining the forward pass
+  def forward(self, x):
+      x = self.cnn_layers(x)
+      x = x.view(x.size(0), -1)
+      x = self.linear_layers(x)
+      return x
+
+model = Net()
+# defining the optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+# defining the loss function
+criterion = nn.CrossEntropyLoss()
+# checking if GPU is available
+if torch.cuda.is_available():
+  model = model.cuda()
+  criterion = criterion.cuda()
+
+print(model)
+
+for i in range(10):
+    running_loss = 0
+    for images, labels in train_loader:
+
+        if torch.cuda.is_available():
+            images = images.cuda()
+            labels = labels.cuda()
+
+        # Training pass
+        optimizer.zero_grad()
+
+        output = model(images)
+        cross_entropy = nn.CrossEntropyLoss()
+        loss = cross_entropy(output, labels)
+
+        # This is where the model learns by backpropagating
+        loss.backward()
+
+        # And optimizes its weights here
+        optimizer.step()
+
+        running_loss += loss.item()
+    print("Epoch {} - Training loss: {}".format(i + 1, running_loss / len(train_loader)))
+
+
